48849af607f53729a1157b88b96987ff66f996c4
nirmal-suresh <nirmal97.suresh@gmail.com>
2021-05-07 15:38:34 +0530

    import views

--- saleor/urls.py
+++ saleor/urls.py
@@ -9,6 +9,7 @@ from .graphql.api import schema
 from .graphql.views import GraphQLView
 from .plugins.views import handle_plugin_webhook
 from .product.views import digital_product
+from .core import views
 
 urlpatterns = [
     url(r"^graphql/", csrf_exempt(GraphQLView.as_view(schema=schema)), name="api"),
