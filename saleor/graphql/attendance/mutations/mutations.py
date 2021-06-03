81a4faf4454e1f67ab03bc139da2cf3d611fa00e
nirmal-suresh <nirmal97.suresh@gmail.com>
2021-06-03 23:31:23 +0530

    added error and isMarked to attendance mutation

--- saleor/graphql/attendance/mutations/mutations.py
+++ saleor/graphql/attendance/mutations/mutations.py
@@ -1,3 +1,4 @@
+from graphene.types.scalars import Boolean
 from saleor import attendance
 from ...core.connection import CountableDjangoObjectType
 from ....attendance.models import Attendance
@@ -8,44 +9,45 @@ from django.conf import settings
 from datetime import datetime
 from . import verify
 import logging
+from django.core.exceptions import ValidationError
 
 logger = logging.getLogger(__name__)
 
 class AttendanceType(CountableDjangoObjectType):
-    isMarked = graphene.Boolean(description="tells wether attendance was marked or not.")
     class Meta:
         model = Attendance
-    @staticmethod
-    def resolve_isMarked(root: Attendance, _info):
-        if root is None:
-            return False
-        else:
-            return True
 
 class MarkAttendance(graphene.Mutation):
+
     class Arguments:
         hash = graphene.String(required=True, description="hash from the qr code.")
     attendance = graphene.Field(AttendanceType)
+    is_marked = graphene.Boolean()
+    error = graphene.String(default_value=None)
     @classmethod
     def mutate(cls, root, info, hash=None):
         user = info.context.user
         date = datetime.today().date()
+        error = None
 
         #check if attendance is already marked
         attendance = Attendance.objects.filter(user=user).filter(date=date).first()
         if attendance is not None:
             logger.debug("attendance already marked")
-            return MarkAttendance(attendance=attendance)
+            return MarkAttendance(attendance=attendance, is_marked=True)
         # verify hash
         date = datetime.today().date()
         if verify(hash):
             # mark attendance
             attendance = Attendance(user=user, date=date)
             attendance.save()
+            is_marked = True
         else:
             logger.debug("Invalid hash")
             attendance = None
+            is_marked = False
+            error = "Invalid hash"
             
-        return MarkAttendance(attendance=attendance)
+        return MarkAttendance(attendance=attendance, is_marked=is_marked, error=error)
 class AttendanceMutations(graphene.ObjectType):
     mark_attendance = MarkAttendance.Field()
